5dc4a1c89247318af68a067a043c2b750e54e8a6
Raghav <Raghavutreja3@gmail.com>
2026-06-01 16:13:20 +0530

    added styling of Page Not Found

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>
-      <h1>404-Page Not Found</h1>
-      <p>The Page You Are Looking For Doesnot Exists</p>
+    <div style={{marginTop:"10%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}} >
+        <div style={{border:"1px solid red",padding:"30px",borderRadius:"20px",backgroundColor:"rgb(255,174,174)"}} > 
+             <h1>404-Page Not Found</h1>
+     <b> <p style={{marginLeft:"22px",color:"yellow"}} >The Page You Are Looking For Doesnot Exists</p>
+ </b>
+        </div>
+     
     </div>
   )
 }
