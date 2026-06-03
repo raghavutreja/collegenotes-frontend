fde17f91d334cfa6eea7bab9606e6a90c68779da
Raghav <Raghavutreja3@gmail.com>
2026-06-03 12:36:08 +0530

    added delete button

--- src/components/DailyPosts/PostCard.jsx
+++ src/components/DailyPosts/PostCard.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import './Posts.css'
+import React from "react";
+import "./Posts.css";
+import axios from "axios";
 
-function PostCard({title,Description,ImageLink}) {
+function PostCard({ title, Description, ImageLink }) {
+   const role=localStorage.getItem("role");
+  const handleDeletePost=async()=>{
+    alert("Delete Done Successfully Please Refresh the Page!")
+    await axios.delete(`${import.meta.env.VITE_API_URL}/deletepost/${title}`).then(()=>{
+      console.log("deleted Successfully")
+    })
+  }
   return (
-    <div className='PostStyle' >
+    <div className="PostStyle">
       <h3>{title}</h3>
-      <p>{Description}</p>
-      <img style={{width:"100%"}} src={ImageLink} alt="Image Not Loaded Properly PLease Try Again Later" />
+      <p>{Description} {
+          role==="admin"?
+            <button
+        onClick={handleDeletePost}
+        style={{
+          marginLeft:"10%",
+          textDecoration: "none",
+          borderRadius: "10px",
+          border: "0.5px solid black",
+          color: "white",
+          backgroundColor: "rgb(53,70,86)",
+        }}
+      >
+        Delete
+      </button>: <p></p>
+        } </p>{" "}
+     
+      <img
+        style={{ width: "100%" }}
+        src={ImageLink}
+        alt="Image Not Loaded Properly PLease Try Again Later"
+      />
     </div>
-  )
+  );
 }
 
-export default PostCard
+export default PostCard;
--- src/components/Papers/Card.jsx
+++ src/components/Papers/Card.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import './Papers.css'
+import axios from 'axios'
 
 function Card({title, Description ,Downloadlink}) {
+   const role=localStorage.getItem("role");
+  const handleDelete=()=>{
+    try{
+    console.log("Delete clicked")
+    alert("Delete Done Please Refresh the Page")
+    axios.delete(`${import.meta.env.VITE_API_URL}/delpaper/${title}`).then(()=>{
+      console.log("Deleted Successfully");
+      
+    })}catch(err){
+      console.log(err);
+    }
+   
+  }
   return (
     <div className='CardOuterBorder' >
       <h6>{title}</h6>
       <p>{Description}</p>
       <a href={Downloadlink} target='_blank' > Download </a>
-    </div>
+      {
+        role==="admin"?
+             <button onClick={handleDelete} style={{marginLeft:"30px",textDecoration:"none",borderRadius:"10px",border:"0.5px solid black",color:"white",backgroundColor:"rgb(53,70,86)"}} >Delete</button>
+ :<p></p>
+      }
+      </div>
   )
 }
 
